087554ee630e19859f1a888c2ec379e392a67dff
Luc Lagarde <matieu.lagarde@gmail.com>
2019-09-28 13:32:19 -0500

    Add test user to database initialization

--- init.sql
+++ init.sql
@@ -6,3 +6,5 @@ CREATE TABLE `users` (
   `data` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=latin1;
+
+INSERT INTO `dev`.`users` (`id`, `username`, `password`, `data`) VALUES ('1', 'test', 'test', '{}');
