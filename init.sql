2f43e1056e686e17c05977e72504338ca6e29e04
Luc Lagarde <matieu.lagarde@gmail.com>
2019-10-10 12:48:47 -0500

    Add posts table to init.sql

--- init.sql
+++ init.sql
@@ -11,3 +11,12 @@ CREATE TABLE `users` (
 /* Add default users */
 INSERT INTO `users` (`id`, `username`, `password`, `data`) VALUES
   ('1', 'test', 'test', '{}');
+
+/* Add posts table */
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `userID` TEXT NOT NULL, /* TODO: This should probably be linked to the `users` table somehow... */
+  `data` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=latin1;
